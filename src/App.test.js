9dc42ba70fb2ee234bd8169ac13e10cd696ed127
amna0488 <amnasalahudin123@gmail.com>
2022-11-10 22:58:18 +0500

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Task3 from './Task3';
 
+//task1
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId("comp");
   expect(linkElement).toBeInTheDocument();
+});
+
+//task2
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("comp");
+  expect(linkElement).toHaveTextContent("Hello World");
+
+});
+//task3
+test('renders learn react link', () => {
+  render(<Task3 />);
+  const linkElement = screen.getByTestId("comp1");
+  
+  expect(linkElement).toHaveTextContent("Mars") 
 });
---
+++ src/Task3.js
@@ -0,0 +1,17 @@
+
+
+
+function Task3() {
+  return (
+    <div data-testid="comp1" >
+      <h1>Hello</h1>
+      <h2>World</h2>
+      <h3>Universe</h3>
+      <h4>Mars</h4>
+     
+      
+    </div>
+  );
+}
+
+export default Task3;
